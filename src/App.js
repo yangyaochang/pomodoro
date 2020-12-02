b74175de15e369d36da61ec8e3e6367f41bf883b
James Yang <ycyang218@gmail.com>
2020-12-01 19:18:42 -0500

    Testing count down timer

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
 
 function App() {
+  const [remainTime, setRemainTime] = React.useState(20)
+  const [countDown, setCountDown] = React.useState(false)
+
+  React.useEffect(() => {
+    if (countDown) {
+      if (remainTime === 0) {return}
+
+      const timer = setTimeout(() => {
+        setRemainTime((currentTime) => currentTime - 1)
+      }, 1000)
+
+      return () => {
+        clearTimeout(timer)
+      }
+    }
+  }, [countDown, remainTime])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={ () => {setCountDown(true)} }>Play</button>
+      <div>
+        <div>{ new Date(remainTime * 1000).toISOString().substr(14, 5) }</div>
+      </div>
     </div>
   );
 }
